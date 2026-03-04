211fc2261a99fa58a08da386c9a5eeb444b717c0
Neptune990 <460219@bsd48.org>
2026-03-04 09:48:13 -0800

    Initialized and some basic steps

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <iomanip>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+int main () {
+  return 0;
+}
+
+//Steps to complete project:
+//1.BUILD A STACK USING A LINKED LIST. You don't need a separate class for this, but you DO need the push(), pop(), and peek() functions.
+//2.BUILD A QUEUE USING A LINKED LIST. You don't need a separate class for this, but you DO need the enqueue() and dequeue() functions.
+//3.1 and 2 will both be in main.cpp
+//4.BUILD A BINARY TREE NODE CLASS WITH A LEFT AND RIGHT POINTER. (This is very similar to the Node class for linked list, except it has a data value, left pointer, and right pointer. If you want to combine the linked list node into the binary tree node, meaning it has a left, right, AND next, you may.)
+//5.most likely will do remaining tree in other classes for organization
+//6.make sure all 3 contain the instructions of overview with mathematical notation, and output infix, prefix, and postfix notation
+//7. MAJOR NOTE: we need to output the entire expression tree for infix, postfix, and prefix -> :( 
+//8. Comments!
