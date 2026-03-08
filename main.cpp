f0536793fa443bcbc10937e8d72148997cc2f8d6
Neptune990 <460219@bsd48.org>
2026-03-07 17:25:52 -0800

    Most of layout + stack and queue functions

--- main.cpp
+++ main.cpp
@@ -4,9 +4,80 @@
 #include <cmath>
 #include <fstream>
 using namespace std;
+//compared to other projects, doing 1 file to see if this a system I can do otherwise by next project I'll revert back to working on multiple files
+// Node class
+class Node {
+public:
+  char data;
+  Node* next;
+  Node* left;
+  Node* right;
+
+  Node(char d) {
+    data = d;
+    next = NULL;
+    left = NULL;
+    right = NULL;
+  }
+};
+//STACK FUNCTIONS
+void push(Node*& top, Node* n);
+Node* pop(Node*& top);
+Node* peek(Node* top);
+//QUEUE FUNCTIONS
+void enqueue(Node*& front, Node*& rear, Node* n);
+Node* dequeue(Node*& front, Node*& rear);
+
+int precedence(char op);
+bool isOperator(char c);
 
 int main () {
   return 0;
+}
+
+void push(Node*& top, Node* n) {
+  n->next = top;
+  top = n;
+}
+
+Node* pop(Node*& top) {
+  if (top == NULL) {
+    return NULL;
+  }
+  Node* temp = top;
+  top = top->next;
+  temp->next = NULL;
+  return temp;
+}
+
+Node* peek(Node* top) {
+  return top;
+}
+
+void enqueue(Node*& front, Node*& rear, Node* n) {
+  if (rear == NULL) {
+    front = rear = n;
+  } 
+  else {
+    rear->next = n;
+    rear = n;
+  }
+}
+
+Node* dequeue(Node*& front, Node*& rear) {
+  if (front == NULL) {
+    return NULL;
+  }
+
+  Node* temp = front;
+  front = front->next;
+
+  if (front == NULL) {
+    rear = NULL;
+  }
+
+  temp->next = NULL;
+  return temp;
 }
 
 //Steps to complete project:
